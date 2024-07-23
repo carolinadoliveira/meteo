8e732604e143b24e5e3abe4ad97c9fc0ce223e63
Carolina Oliveira <98764896+carolinadoliveira@users.noreply.github.com>
2024-07-22 18:31:30 -0700

    injected the forecast html from JS

--- src/index.js
+++ src/index.js
@@ -10,8 +10,6 @@ function refreshWeather(response) {
 
   let iconElement = document.querySelector("#icon");
 
-  console.log(response);
-
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
@@ -55,7 +53,34 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+ <div class="wather-forecast-day">
+   <div class="weather-forecast-date">${day}</div>
+   <div class="weather-forecast-icon">🌧️</div>
+   <div class="weather-forecast-temperatures">
+     <div class="weather-forecast-temperature">
+       <strong>15°</strong>
+     </div>
+     <div class="weather-forecast-temperature">9°</div>
+   </div>
+ </div>
+ `;
+  });
+
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Phoenix");
+displayForecast();
